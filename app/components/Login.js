767999f17b33d04800a83b7154a8a5b3986508b7
Austen Ma <austenma@Austens-MacBook-Pro.local>
2017-08-29 19:33:30 -0700

    Fixed some conflicts

--- app/components/Login.js
+++ app/components/Login.js
@@ -143,7 +143,30 @@ const styles = StyleSheet.create({
  },
  credentials: {
    paddingTop: 20
+<<<<<<< HEAD
  }
+=======
+ },
+  container: {
+    alignItems: 'center',
+    marginTop: '20%',
+  },
+  title: {
+    fontWeight: 'bold'
+  },
+  inputFields: {
+    borderWidth: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 20,
+    width: '50%',
+    marginTop: 20,
+    marginLeft: '20%'
+  },
+  credentials: {
+    paddingTop: 20
+  }
+>>>>>>> Joe's minor pull incoming
 });
 
 
