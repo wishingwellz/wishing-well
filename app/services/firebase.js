c308ab82fe398c21172c8fc9be204c543ba2f7c6
Joseph Cheng <joecheng950103@gmail.com>
2017-08-29 19:28:19 -0700

    made minor styling changes and firebase db insertion method

--- app/services/firebase.js
+++ app/services/firebase.js
@@ -1,5 +1,5 @@
 import * as Firebase from "firebase"
-import { apiKey, authDomain, databaseURL} from '../config'
+import { apiKey, authDomain, databaseURL} from '../../config'
 
 const firebaseConfig = {
   apiKey,
